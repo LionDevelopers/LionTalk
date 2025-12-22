f6685c929734cc4d08c1471c7c05c6c19121b920
Andrew Hsu <ah4413@columbia.edu>
2025-12-21 23:09:54 -0600

    Working scrape for Stat Seminar Series

--- apps/scraper-api/src/scraper.py
+++ apps/scraper-api/src/scraper.py
@@ -1,34 +1,14 @@
-import requests
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
-def main():
-    url = "https://utexas.edu/"
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo = 50)
+    page = browser.new_page()
+    page.goto('https://stat.columbia.edu/seminars/statistics-seminar-series/')
+    html = page.inner_html('#seminar-content')
+    soup = BeautifulSoup(html, 'html.parser')
 
-    # Header to mimic a human browser request
-    headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Referer": "https://www.google.com/",
-    "DNT": "1", # Do Not Track
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1"
-    }
+    with open('source.txt', 'wb') as f:
+        f.write(soup.encode('utf-8'))
 
-    try:
-        response = requests.get(url, headers=headers)
-        # Raise an error if the request failed (e.g., 404 or 500)
-        response.raise_for_status()
-
-        # Save the content to a file
-        with open("source.txt", "w", encoding="utf-8") as f:
-            f.write(response.text)
-            
-        print("HTML saved successfully to source.txt")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    main()
+    print(f"HTML written to source.txt")
