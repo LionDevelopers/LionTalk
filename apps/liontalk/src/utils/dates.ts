63f6824939de6e97e4152ffb45833d2b15425687
Rhyzhang <ryanzhangofficial@gmail.com>
2026-02-03 16:38:24 -0500

    Date fix

--- apps/liontalk/src/utils/dates.ts
+++ apps/liontalk/src/utils/dates.ts
@@ -1,8 +1,18 @@
 import { Seminar } from '../types';
 
 const MONTH_MAP: { [key: string]: number } = {
-  Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5,
-  Jul: 6, Aug: 7, Sep: 8, Sept: 8, Oct: 9, Nov: 10, Dec: 11
+  Jan: 0, JAN: 0, 
+  Feb: 1, FEB: 1,
+  Mar: 2, MAR: 2, 
+  Apr: 3, APR: 3,
+  May: 4, MAY: 4,
+  Jun: 5, JUN: 5,
+  Jul: 6, JUL: 6, 
+  Aug: 7, AUG: 7, 
+  Sep: 8, SEP: 8, SEPT: 8, 
+  Oct: 9, OCT: 9,
+  Nov: 10, NOV: 10, 
+  Dec: 11, DEC: 11
 };
 
 export const parseSeminarDate = (dateStr: string, timeStr: string) => {
